a9f6adbc682defb1eafee594498c8722b8abd17d
Htgar <development@htgar.org>
2025-07-25 11:27:25 +0800

    Fixed container init script

--- init-container.sh
+++ init-container.sh
@@ -1,9 +1,9 @@
 # Set up dotfiles
 export XDG_CONFIG_HOME="$HOME"/.config
-mkdir -p "XDG_CONFIG_HOME"
+mkdir -p "$XDG_CONFIG_HOME"
 
-ln -sf "$PWD/nvim" "XDG_CONFIG_HOME"/nvim
-ln -sf "$PWD/fish" "XDG_CONFIG_HOME"/fish
+ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
+ln -sf "$PWD/fish" "$XDG_CONFIG_HOME"/fish
 
 # Check if homebrew installed
 if command -v brew >/dev/null 2>&1; then
